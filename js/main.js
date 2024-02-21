b560a03bbc267577b875ecb17b7e66cf690d8269
MaminiainaIndrafo <maminiainaindrafo@gmail.com>
2024-02-21 08:38:35 +0300

    Change position element in head section

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ $(function () {
   $('[data-countdown]').each(function () {
     var $this = $(this), finalDate = $(this).data('countdown');
     $this.countdown(finalDate, function (event) {
-      $this.html(event.strftime('<div class="header-countdown pt-70 d-flex justify-content-center"><div class="single-count-content count-color-1"><span class="count">%D</span><p class="text">Jours</p></div><div class="single-count-content count-color-2"><span class="count">%H</span><p class="text">Heures</p></div><div class="single-count-content count-color-3"><span class="count">%M</span><p class="text">Minutes</p></div><div class="single-count-content count-color-4"><span class="count">%S</span><p class="text">Seconds</p></div></div>'));
+      $this.html(event.strftime('<div class="header-countdown pt-50 d-flex justify-content-center"><div class="single-count-content count-color-1"><span class="count">%D</span><p class="text">Jours</p></div><div class="single-count-content count-color-2"><span class="count">%H</span><p class="text">Heures</p></div><div class="single-count-content count-color-3"><span class="count">%M</span><p class="text">Minutes</p></div><div class="single-count-content count-color-4"><span class="count">%S</span><p class="text">Seconds</p></div></div>'));
     });
   });
 
