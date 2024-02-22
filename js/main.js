2e55ca82bcf739c3a3793b21172629d8328df4c4
MaminiainaIndrafo <maminiainaindrafo@gmail.com>
2024-02-22 15:07:48 +0300

    Change image in header

--- js/main.js
+++ js/main.js
@@ -149,8 +149,8 @@ $(document).ready(function() {
   $("#home").mousemove(function(e){
             var pageX = e.pageX - ($(window).width() / 2);
             var pageY = e.pageY - ($(window).height() / 2);
-            var newvalueX = width * pageX * -1 - 25;
-            var newvalueY = height * pageY * -1 + 10;
+            var newvalueX = width * pageX * -1 - 2;
+            var newvalueY = height * pageY * -1 - 1;
             $('#home').css("background-position", newvalueX+"px "+newvalueY+"px");
   });
   });
