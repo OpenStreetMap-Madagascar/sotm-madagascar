c1801a4bd79d56a0ee480166937b3ff9e4cabc39
MaminiainaIndrafo <maminiainaindrafo@gmail.com>
2024-04-11 07:46:39 +0300

    Change heigth of program

--- js/schedule.js
+++ js/schedule.js
@@ -97,7 +97,7 @@ jQuery(document).ready(function ($) {
 				eventHeight = self.eventSlotHeight * duration / self.timelineUnitDuration;
 
 			$(this).css({
-				top: (eventTop - 1) + 'px',
+				top: (eventTop + 10) + 'px',
 				height: (eventHeight + 10) + 'px'
 			});
 		});
